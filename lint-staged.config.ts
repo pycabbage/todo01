6465db7d70065cbe94a3284246894087c4a5a873
kotaro.higuchi <33714346+pycabbage@users.noreply.github.com>
2025-08-22 21:48:31 +0900

    update lint-staged configu\

--- lint-staged.config.ts
+++ lint-staged.config.ts
@@ -1,6 +1,12 @@
 import type { Configuration } from "lint-staged"
 
 const config: Configuration = {
-  "*": () => ["biome check"],
+  "*.{js,ts,cjs,mjs,d.cts,d.mts,jsx,tsx,json,jsonc}": () => [
+    "biome check --files-ignore-unknown=true", // フォーマットのチェックとリント
+    "biome check --write --no-errors-on-unmatched", // フォーマット、インポート文のソート、リント、安全な修正の適用
+    "biome check --write --unsafe --no-errors-on-unmatched", // フォーマット、インポート文のソート、リント、安全及び安全ではない修正の適用
+    "biome format --write --no-errors-on-unmatched", // フォーマット
+    "biome lint --write --no-errors-on-unmatched", // リントと安全な修正の適用
+  ],
 }
 export default config
